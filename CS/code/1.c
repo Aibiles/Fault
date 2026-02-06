453ff1acbc4aef768eab8c2d742e8700012e74ad
Mimsy <cshi0652@gmail.com>
2026-02-06 16:53:01 +0800

    first commit

---
+++ CS/code/1.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <math.h>
+
+int main(void)
+{
+	char a = '\72';
+	printf("%d \n", sizeof(a));
+	return 0;
+}
---
+++ CS/code/2.c
@@ -0,0 +1,10 @@
+#define _USE_MATH_DEFINES
+#include <stdio.h>
+#include <math.h>
+
+int main(void)
+{
+	int r = 2;
+	printf("%f\n", M_PI*pow(r,2));
+	return 0;
+}
