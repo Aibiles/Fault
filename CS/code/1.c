dbcc5b34a5afa6f9c2e1e3c0f7b277fc76b6a2d6
Mimsy <cshi0652@gmail.com>
2026-02-28 00:20:53 +0800

    2.27

--- CS/code/1.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-#include <math.h>
-
-int main(void)
-{
-	char a = '\72';
-	printf("%d \n", sizeof(a));
-	return 0;
-}
--- CS/code/2.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-#include "test.h"
-
-int main() {
-    printf("a = %d\n", a);
-    printf("add(3, 4) = %d\n", add(3, 4));
-    return 0;
-}
